e227378c5ada508c180211c49d16c2e9593051d6
Brett Bieber <brett.bieber@gmail.com>
2011-03-26 09:00:11 -0500

    Add skeleton for the release broker

---
+++ src/ReleaseBroker/Main.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * PEAR2\ReleaseBroker\Main
+ *
+ * PHP version 5
+ *
+ * @category  Yourcategory
+ * @package   PEAR2_ReleaseBroker
+ * @author    Your Name <handle@php.net>
+ * @copyright 2011 Your Name
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @version   SVN: $Id$
+ * @link      http://svn.php.net/repository/pear2/PEAR2_ReleaseBroker
+ */
+
+/**
+ * Main class for PEAR2_ReleaseBroker
+ *
+ * @category  Yourcategory
+ * @package   PEAR2_ReleaseBroker
+ * @author    Your Name <handle@php.net>
+ * @copyright 2011 Your Name
+ * @license   http://www.opensource.org/licenses/bsd-license.php New BSD License
+ * @link      http://svn.php.net/repository/pear2/PEAR2_ReleaseBroker
+ */
+namespace PEAR2\ReleaseBroker;
+class Main
+{
+}
